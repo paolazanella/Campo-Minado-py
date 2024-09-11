ea16fb48169faf59af90f6a45641fd09d9684cc9
Paola Zanella <pzanella@edu.univali.br>
2024-09-10 22:34:42 -0300

    adicinado a jogabilidade do mapa

--- CampoMinado.py
+++ CampoMinado.py
@@ -39,4 +39,55 @@ def mostrar_mapa(bombas_adjacentes, revelado):  # bombas_adjacentes=BOMBAS NAS V
         print(linha)  # imprime a linha do map
 
 
+def jogo(mapa_bombas):
+    """Controla a lógica principal do jogo Campo Minado."""
+    bombas_adjacentes = calcular_bombas(
+        mapa_bombas)  # calula as bombas vizinhas
+    altura, largura = mapa_bombas.shape  # obtem as dimencoes do mapa
+    # inicializa a matrisz de revelacao
+    revelado = np.zeros((altura, largura), dtype=bool)
 
+    while True:
+        mostrar_mapa(bombas_adjacentes, revelado)  # exibe o mapa atual
+
+        # solicita as posicao
+        while True:
+            try:
+                linha = int(input("digite a linha:"))  # entrada da linha
+                coluna = int(input("digite a colula:"))  # entrada da col
+
+                # verifica se as posicao e valida
+                if linha < 0 or linha >= altura or coluna < 0 or coluna >= largura:
+                    print("coordenadas invalidas!por favor, tentar novamnte")
+                else:
+                    break  # Sai do lool se a possicao e valida
+            except ValueError:
+                print("entrada invalida!por favoe, insera numero interios")
+
+        if mapa_bombas[linha, coluna] == 1:  # se for uma bomba
+            print("voce acertou uma bomba! fim de jogoa")
+            mostrar_resultado(False)  # mostra resuldado de derroda
+            return False  # retorna false indicando que o jogo termino
+        else:
+            revelado[linha, coluna]: True  # revelaa a posicao
+            # se nao tive bombas nas vizinhaca
+            if bombas_adjacentes[linha, coluna] == 0:
+                # revela as posicoes vizinhas
+                for di in [-1, 0, 0, 1]:
+                    for dj in [-1, 0, 1]:
+                        if 0 < linha+di < altura and 0 <= coluna + dj < largura:
+                            revelado[linha + di, coluna + dj] = True
+
+        # verifica se o jogado venceu
+        if np.all(revelado | mapa_bombas):
+            print("Parabéns! Você venceu!")
+            mostrar_resultado(True)  # Mostra o resultado de vitória
+            return True  # Retorna True indicando que o jogo terminou
+
+
+def calcular_bombas(mapa_bombas):
+    """Calcula o número de bombas adjacentes para cada célula."""
+
+
+def mostrar_resultado(vitoria):
+    """Mostra o resultado final do jogo."""
